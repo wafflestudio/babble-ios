1a53c9d7158a97fa6de0f21fa9eb12cbe3925c3a
Chaehyun Park <c.park@snu.ac.kr>
2024-02-08 22:23:01 +0900

    feat: create xcode project

---
+++ babble/babbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  babbleApp.swift
+//  babble
+//
+//  Created by Chaehyun Park on 2024/02/08.
+//
+
+import SwiftUI
+
+@main
+struct babbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
