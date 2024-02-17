c6ca4667334c1c6d6cf8f43b3531a92361c02524
impri2 <impri@naver.om>
2024-02-17 22:38:07 +0900

    feat: room enter view design change

--- babble/chat/RoomEnterView.swift
+++ babble/chat/RoomEnterView.swift
@@ -15,19 +15,23 @@ struct RoomEnterView: View {
     let onBackButtonPressed:()->()
     var body: some View {
         VStack(alignment: .center){
+            ChatHeaderView(name: viewModel.chatroom.name, tag: viewModel.chatroom.hashTag, members_count: viewModel.chatterCount, onBackButtonPressed: {
+                onBackButtonPressed()
+            })
             Text("채팅방 참여하기")
-            Button{
+         /*   Button{
                 onBackButtonPressed()
             }label:{
                 Image(systemName: "chevron.left")
-            }
+            }*/
             TextField("닉네임", text: $nickname, axis: .vertical)    .background(RoundedRectangle(cornerRadius: 5).fill(Color("Blue5")))
+                .padding()
             Button{
                 viewModel.enterRoom(nickname: nickname)
             }label:{
                 Text("입장")
             }
-        }.frame(minWidth:0,maxWidth: .infinity,minHeight: 0,maxHeight: .infinity,alignment: .topLeading).padding()
+        }.frame(minWidth:0,maxWidth: .infinity,minHeight: 0,maxHeight: .infinity,alignment: .topLeading)
     }
 }
 
